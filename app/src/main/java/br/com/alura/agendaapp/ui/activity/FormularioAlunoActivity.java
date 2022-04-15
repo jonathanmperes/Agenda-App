92634e83ace67e6b3054b57e52f557d92f7232d4
jonathanmperes <emailmarceloperes@gmail.com>
2022-04-15 16:20:40 -0300

    Criando activity para adicionar contatos na agenda

---
+++ app/src/main/java/br/com/alura/agendaapp/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,16 @@
+package br.com.alura.agendaapp.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.alura.agendaapp.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
